fe1d067eb9632ca80b1d3095c0b173787e7396f6
Jordan Victor <jordanvictor63@gmail.com>
2026-01-07 16:17:50 -0400

    Função de datas do módulo de edição dos estabelecimentos que mostra a tabela de dias da semana e horários finalizada

--- src/components/estabelecimentos/EditEstabelecimento.vue
+++ src/components/estabelecimentos/EditEstabelecimento.vue
@@ -31,39 +31,62 @@
                                 </div>
                                 
                                 <!-- Horários e dias -->
-                                <div class="mt-3">
+                                <div class="mt-3 overflow-x-scroll">
                                     <table class="table border">
                                         <thead style="background-color: gray;">
                                             <tr>                                                
                                                 <th 
                                                 class="text-secondary text-center" 
                                                 v-bind:class="new Date().getDay() == 1 ? 'text-success' : ''">SEG</th>
-                                                <th class="text-secondary text-center">TER</th>
-                                                <th class="text-secondary text-center">QUA</th>
-                                                <th class="text-secondary text-center">QUI</th>
-                                                <th class="text-secondary text-center">SEX</th>
-                                                <th class="text-secondary text-center">SÁB</th>
-                                                <th class="text-secondary text-center">DOM</th>
+                                                
+                                                <th 
+                                                class="text-secondary text-center"
+                                                v-bind:class="new Date().getDay() == 2 ? 'text-success' : ''">TER</th>
+                                                <th 
+                                                class="text-secondary text-center"
+                                                v-bind:class="new Date().getDay() == 3 ? 'text-success' : ''">QUA</th>
+
+                                                <th 
+                                                class="text-secondary text-center"
+                                                v-bind:class="new Date().getDay() == 4 ? 'text-success' : ''">QUI</th>
+
+                                                <th 
+                                                class="text-secondary text-center"
+                                                v-bind:class="new Date().getDay() == 5 ? 'text-success' : ''">SEX</th>
+
+                                                <th 
+                                                class="text-secondary text-center"
+                                                v-bind:class="new Date().getDay() == 6 ? 'text-success' : ''">SÁB</th>
+
+                                                <th class="text-secondary text-center"
+                                                v-bind:class="new Date().getDay() == 0 ? 'text-success' : ''">DOM</th>
                                             </tr>
                                         </thead>
 
                                         <tbody>
                                             <tr>
                                                 <!-- Horários de segunda -->
-                                                <td class="border">
+                                                <td class="border">                                                 
                                                     <div
+                                                    v-if="estabelecimento.dias_func.find(d=>d.dia == 'segunda-feira')"
                                                     class="mb-2" 
                                                     v-for="(dia, i) in estabelecimento.dias_func.find(d=>d.dia == 'segunda-feira')?.horarios || []"
                                                     >
                                                         <div style="width: 40px; font-size: 13px; text-align: center; line-height: 1;">
                                                             {{ dia.horaIni }} às {{ dia.horaFim }}
                                                         </div>
-                                                    </div>
+                                                    </div>    
+                                                    
+                                                    <div v-else>
+                                                        -
+                                                    </div>                                                  
                                                 </td>
+
 
                                                 <!-- Horários de terça -->
                                                 <td class="border">
                                                     <div
+                                                    v-if="estabelecimento.dias_func.find(d=>d.dia == 'terça-feira')"
                                                     class="mb-2" 
                                                     v-for="(dia, i) in estabelecimento.dias_func.find(d=>d.dia == 'terça-feira')?.horarios || []"
                                                     >
@@ -71,17 +94,101 @@
                                                             {{ dia.horaIni }} às {{ dia.horaFim }}
                                                         </div>
                                                     </div>
+
+                                                    <div v-else>
+                                                        -
+                                                    </div>
                                                 </td>
                                                 
+
                                                 <!-- Horários de quarta -->
-                                                 <td class="border">
+                                                <td 
+                                                v-bind:class="new Date().getDay() == 3 ? 'border-success' : 'border'"
+                                                >
                                                     <div
+                                                    v-if="estabelecimento.dias_func.find(d=>d.dia == 'quarta-feira')"
                                                     class="mb-2" 
                                                     v-for="(dia, i) in estabelecimento.dias_func.find(d=>d.dia == 'quarta-feira')?.horarios || []"
                                                     >
                                                         <div style="width: 40px; font-size: 13px; text-align: center; line-height: 1;">
                                                             {{ dia.horaIni }} às {{ dia.horaFim }}
                                                         </div>
+                                                    </div>
+
+                                                    <div v-else>
+                                                        -
+                                                    </div>
+                                                </td>
+
+
+                                                <!-- Horários de quinta -->
+                                                <td class="border">
+                                                    <div
+                                                    v-if="estabelecimento.dias_func.find(d=>d.dia == 'quinta-feira')"
+                                                    class="mb-2" 
+                                                    v-for="(dia, i) in estabelecimento.dias_func.find(d=>d.dia == 'quinta-feira')?.horarios || []"
+                                                    >
+                                                        <div style="width: 40px; font-size: 13px; text-align: center; line-height: 1;">
+                                                            {{ dia.horaIni }} às {{ dia.horaFim }}
+                                                        </div>
+                                                    </div>
+
+                                                    <div v-else>
+                                                        -
+                                                    </div>
+                                                </td>
+
+
+                                                <!-- Horários de sexta -->
+                                                <td class="border">
+                                                    <div
+                                                    v-if="estabelecimento.dias_func.find(d=>d.dia == 'sexta-feira')"
+                                                    class="mb-2" 
+                                                    v-for="(dia, i) in estabelecimento.dias_func.find(d=>d.dia == 'sexta-feira')?.horarios || []"
+                                                    >
+                                                        <div style="width: 40px; font-size: 13px; text-align: center; line-height: 1;">
+                                                            {{ dia.horaIni }} às {{ dia.horaFim }}
+                                                        </div>
+                                                    </div>
+
+                                                    <div v-else>
+                                                        -
+                                                    </div>
+                                                </td>
+
+
+                                                <!-- Horários de sábado -->
+                                                <td class="border">
+                                                    <div
+                                                    v-if="estabelecimento.dias_func.find(d=>d.dia == 'sábado')"
+                                                    class="mb-2" 
+                                                    v-for="(dia, i) in estabelecimento.dias_func.find(d=>d.dia == 'sábado')?.horarios || []"
+                                                    >
+                                                        <div style="width: 40px; font-size: 13px; text-align: center; line-height: 1;">
+                                                            {{ dia.horaIni }} às {{ dia.horaFim }}
+                                                        </div>
+                                                    </div>
+
+                                                    <div v-else>
+                                                        -
+                                                    </div>
+                                                </td>
+
+
+                                                <!-- Horários de sábado -->
+                                                <td class="border">
+                                                    <div
+                                                    v-if="estabelecimento.dias_func.find(d=>d.dia == 'domingo')"
+                                                    class="mb-2" 
+                                                    v-for="(dia, i) in estabelecimento.dias_func.find(d=>d.dia == 'domingo')?.horarios || []"
+                                                    >
+                                                        <div style="width: 40px; font-size: 13px; text-align: center; line-height: 1;">
+                                                            {{ dia.horaIni }} às {{ dia.horaFim }}
+                                                        </div>
+                                                    </div>
+
+                                                    <div v-else>
+                                                        -
                                                     </div>
                                                 </td>
                                             </tr>
@@ -170,54 +277,38 @@
                 }
 
 
-                let horaAgora = agora.getHours() * 60 + agora.getMinutes()
-                // horaAgora = 10*60 + 1
-                
-                // Comparando horários do dia de hoje para verificar se o etsbelecimento está aberto ou não
-                estabelecimentos.value.forEach(estabelecimento=>{
-                    var aberto = false
+                setInterval(()=>{
+                    console.log("oiii")
+                    let horaAgora = new Date().getHours() * 60 + new Date().getMinutes()
+                    // horaAgora = 10*60 + 1
+                    
+                    // Comparando horários do dia de hoje para verificar se o etsbelecimento está aberto ou não
+                    estabelecimentos.value.forEach(estabelecimento=>{
+                        var aberto = false
 
-                    estabelecimento.dias_func.forEach(dia_func=>{
-                        // Se o dia da semana for igual a uns dos dias da semana do estabelecimento
-                        if(dia_func.dia == diaSemHoje){
-                            // Comparando hora´rio agora com os horários de funcionamento
-                            dia_func.horarios.forEach((horario, i)=>{
-                                // Hora e minuto (início e fim) do estabelecimento hoje
-                                var horaIni = Number(horario.horaIni.split(':')[0])*60 + Number(horario.horaIni.split(':')[1])                                
-                                var horaFim = Number(horario.horaFim.split(':')[0])*60 + Number(horario.horaFim.split(':')[1])
-                               console.log(horaIni + "-" + horaAgora + "-" +horaFim)
-                                // Se existir um próximo intervalo de horários
-                                if(dia_func.horarios[i+1]){
-                                    // Hora inicial em minutos do próximo intervalo
-                                    var horaIniProxIntervalo = Number(dia_func.horarios[i+1].horaIni.split(':')[0])*60 + Number(dia_func.horarios[i+1].horaIni.split(':')[1])
-                                     
-                                    // Verificando se o horário agora está fora de algum intervalo de horários
-                                    if(
-                                        horaAgora > horaFim && 
-                                        horaAgora < horaIniProxIntervalo 
-                                    ){
-                                        aberto = false
-                                        return aberto
-                                    }
-                                    else{
-                                        aberto = true
-                                        return aberto
-                                    }
-                                }
-                                else{
+                        estabelecimento.dias_func.forEach(dia_func=>{
+                            // Se o dia da semana for igual a uns dos dias da semana do estabelecimento
+                            if(dia_func.dia == diaSemHoje){
+                                // Comparando hora´rio agora com os horários de funcionamento
+                                dia_func.horarios.forEach((horario, i)=>{
+                                    // Hora e minuto (início e fim) do estabelecimento hoje
+                                    var horaIni = Number(horario.horaIni.split(':')[0])*60 + Number(horario.horaIni.split(':')[1])                                
+                                    var horaFim = Number(horario.horaFim.split(':')[0])*60 + Number(horario.horaFim.split(':')[1])
+                                console.log(horaIni + "-" + horaAgora + "-" +horaFim)
+
+                                    // Se existir um próximo intervalo de horários    
                                     if(horaAgora >= horaIni && horaAgora <= horaFim){
                                         aberto = true
                                         return aberto
-                                    }    
-                                }                                           
-                            })
-                        }
-                    })
+                                    }                                                                         
+                                })
+                            }
+                        })
 
-                    console.log(aberto)
-                    estabelecimento.aberto = aberto
-                })
-                   
+                        console.log(aberto)
+                        estabelecimento.aberto = aberto
+                    })
+                }, 1000)   
                 
                 
 
